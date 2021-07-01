62a9a90e7f35783bd042e9fd1e5c77e2e2eb7c8c
marina-stack <80834133+marina-stack@users.noreply.github.com>
2021-07-01 15:18:31 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ import java.nio.file.*;
 
 class Main {
     public static void main (String [] args) {
-        String [] arab = new String [] {"10","1","2","3","4","5","6","7","8","9"};
-        String [] rome = new String [] {"X","I","II","III","IV","V","VI","VII","VIII","IX"};
-        String s1, sign , s3 ;
+        String [] arab = new String [] {"10","1","2","3","4","5","6","7","8","9", "20", "30", "40", "50", "60", "70", "80", "90", "100"};
+        String [] rome = new String [] {"X","I","II","III","IV","V","VI","VII","VIII","IX", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"};
+        String s1 = null, sign = null, s3 = null;
         int a1 = 0, b1=0;
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("inlet.txt"));
              BufferedReader br = new BufferedReader (new InputStreamReader(System.in))) {
@@ -25,54 +25,50 @@ class Main {
                 if (rome[i].equals(s1)) {
                     s1 = arab[i];
                 }
-                else if (rome[i].equals(sign)) {
-                    sign = arab[i];
-                }
                 else if (rome[i].equals(s3)) {
                     s3 = arab[i];
                 }
             }
-            a1 = Integer.parseInt(s1.trim());
-            b1 = Integer.parseInt(s3.trim());
-
-            if (((a1>0) & (a1<=10)) & ((b1>0) & (b1<=10))) {
-                switch (sign) {
-                    case "+" -> {
-                        Addition v1 = new Addition();
-                        v1.a = a1;
-                        v1.b = b1;
-                        v1.displayInfo();
-                    }
-                    case "-" -> {
-                        Subtraction v2 = new Subtraction();
-                        v2.a = a1;
-                        v2.b = b1;
-                        v2.displayInfo();
-                    }
-                    case "*" -> {
-                        Multiplication v3 = new Multiplication();
-                        v3.a = a1;
-                        v3.b = b1;
-                        v3.displayInfo();
-                    }
-                    case "/" -> {
-                        Division v4 = new Division();
-                        v4.a = a1;
-                        v4.b = b1;
-                        v4.displayInfo();
-                    }
-                    default -> System.out.println("Введен неверный знак");
-            }
-
-            }
-            else {
-                System.out.println ("Введено неверное число");
-            }
-
-
         }
         catch(IOException ex) {
             System.out.println("Errors!!!");
+        }
+
+        a1 = Integer.parseInt(s1.trim());
+        b1 = Integer.parseInt(s3.trim());
+
+        if (((a1>0) & (a1<=10)) & ((b1>0) & (b1<=10))) {
+            switch (sign) {
+                case "+" -> {
+                    Addition v1 = new Addition();
+                    v1.a = a1;
+                    v1.b = b1;
+                    v1.displayInfo();
+                }
+                case "-" -> {
+                    Subtraction v2 = new Subtraction();
+                    v2.a = a1;
+                    v2.b = b1;
+                    v2.displayInfo();
+                }
+                case "*" -> {
+                    Multiplication v3 = new Multiplication();
+                    v3.a = a1;
+                    v3.b = b1;
+                    v3.displayInfo();
+                }
+                case "/" -> {
+                    Division v4 = new Division();
+                    v4.a = a1;
+                    v4.b = b1;
+                    v4.displayInfo();
+                }
+                default -> System.out.println("Введен неверный знак");
+            }
+
+        }
+        else {
+            System.out.println ("Введено неверное число");
         }
     }
 }
@@ -104,7 +100,3 @@ class Division {
         System.out.print(a / b);
     }
 }
-
-
-
-
